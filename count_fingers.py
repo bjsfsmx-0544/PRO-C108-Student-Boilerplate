d963f5222cc37aa0b84d6f2a94a4a5fb7574a4dd
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-03-02 22:40:17 -0600

    Traducción 1.0
    Modificación de comentarios y elementos en pantalla. Actualización de README.md

--- count_fingers.py
+++ count_fingers.py
@@ -10,20 +10,20 @@ hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5
 
 tipIds = [4, 8, 12, 16, 20]
 
-# Define a function to count fingers
+# Definir una función para contar dedos
 def countFingers(image, hand_landmarks, handNo=0):
     print()
            
     ####################################################
 
-        # ADD CODE HERE
+    # AGREGAR CÓDIGO AQUÍ
 
     ####################################################
 
-# Define a function to 
+# Definir una función para
 def drawHandLanmarks(image, hand_landmarks):
 
-    # Darw connections between landmark points
+    # Dibujar conexiones entre los puntos de referencia
     if hand_landmarks:
 
       for landmarks in hand_landmarks:
@@ -36,23 +36,23 @@ while True:
 
     image = cv2.flip(image, 1)
     
-    # Detect the Hands Landmarks 
+    # Detectar los puntos de referencia de las manos
     results = hands.process(image)
 
-    # Get landmark position from the processed result
+    # Obtener la posición de los puntos de referencia del resultado procesado
     hand_landmarks = results.multi_hand_landmarks
 
-    # Draw Landmarks
+    # Dibujar puntos de referencia
     drawHandLanmarks(image, hand_landmarks)
 
-    # Get Hand Fingers Position        
+    # Obtener la posición de los dedos de la mano
     ##################
-    # ADD CODE HERE
+    # AGREGAR CÓDIGO AQUÍ
     ##################
 
-    cv2.imshow("Media Controller", image)
+    cv2.imshow("Controlador de medios", image)
 
-    # Quit the window on pressing Sapcebar key
+    # Cerrar la ventana al presionar la barra espaciadora
     key = cv2.waitKey(1)
     if key == 32:
         break
